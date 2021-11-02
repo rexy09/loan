b4a3e38226a272b9c554d131263a87d8958f4f08
prince mutungi <princeherman08@gmail.com>
2021-11-02 17:36:08 +0300

    Initial USSD Configurations

--- loan/settings.py
+++ loan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_y@0zn86-f!0*!xv2es_$1i@$^0i8+gb*!9r91n&dces7%lyg#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
